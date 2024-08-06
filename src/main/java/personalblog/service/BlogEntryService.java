01b4f7b6d47cc2da6ce749fee4c16663be12663d
cescarez <christabel.escarez@gmail.com>
2024-08-05 21:56:25 -0700

    Change user, url, move connection during instantiation to method calls, add sql statement delimiter

--- src/main/java/personalblog/service/BlogEntryService.java
+++ src/main/java/personalblog/service/BlogEntryService.java
@@ -1,53 +1,42 @@
 package personalblog.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import personalblog.data.BlogEntry;
 
 import java.sql.*;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 @Service
 public class BlogEntryService {
-    Connection conn;
-
-    public BlogEntryService() {
-        try {
-            String ec2Port = "3.81.98.94:8080";
-            String url = "jdbc:postgresql://localhost/test";
-            Properties props = new Properties();
-            props.setProperty("user", "dbuser");
-            props.setProperty("password", "secret");
-            props.setProperty("ssl", "true");
-            props.setProperty("options", "-c search_path=test,public,pg_catalog -c statement_timeout=90000");
-            Connection conn = DriverManager.getConnection(url, props);
-        } catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public List<BlogEntry> getAllBlogEntries() {
         List<BlogEntry> blogEntries = new ArrayList<>();
-        String sqlQuery = "SELECT * FROM blog_entry";
+        String sqlQuery = "SELECT * FROM blog_entry;";
         try {
+            String url = "jdbc:postgresql://localhost:5432/blog";
+            Connection conn = DriverManager.getConnection(url, "postgres", "postgres");
             Statement statement = conn.createStatement();
+
             ResultSet resultSet = statement.executeQuery(sqlQuery);
             while (resultSet.next()) {
+
+                System.out.println(resultSet);
+                System.out.println(resultSet.toString());
                 BlogEntry retrievedBlogEntry =
                         new BlogEntry(
-                                resultSet.getString("blog_entry_id"),
+                                resultSet.getString("id"),
                                 resultSet.getTimestamp("created_at").toInstant(),
                                 resultSet.getString("blog_id"),
                                 resultSet.getString("title"),
                                 resultSet.getString("content"),
                                 resultSet.getBoolean("is_published"));
                 blogEntries.add(retrievedBlogEntry);
+                System.out.println(retrievedBlogEntry);
             }
+
+            System.out.println(resultSet.getFetchSize());
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -55,14 +44,16 @@ public class BlogEntryService {
     }
 
     public BlogEntry getBlogEntryById(String blogEntryId) {
-        String sqlQuery = String.format("SELECT * FROM blog_entry WHERE blog_entry_id = '%s'", blogEntryId);
+        String sqlQuery = String.format("SELECT * FROM blog_entry WHERE id = '%s';", blogEntryId);
         BlogEntry blogEntry = null;
         try {
+            String url = "jdbc:postgresql://localhost:5432/blog";
+            Connection conn = DriverManager.getConnection(url, "postgres", "postgres");
             Statement statement = conn.createStatement();
             ResultSet resultSet = statement.executeQuery(sqlQuery);
             if (resultSet.next()) {
                 blogEntry = new BlogEntry(
-                        resultSet.getString("blog_entry_id"),
+                        resultSet.getString("id"),
                         resultSet.getTimestamp("created_at").toInstant(),
                         resultSet.getString("blog_id"),
                         resultSet.getString("title"),
@@ -76,17 +67,19 @@ public class BlogEntryService {
     }
 
     public BlogEntry createBlogEntry(BlogEntry blogEntry) {
-        String sqlQuery = String.format("INSERT INTO blog_entry (blog_id,title,content,is_published)\nVALUES('%s','%s','%s','%s')", blogEntry.getBlogEntryId(), blogEntry.getTitle(), blogEntry.getContent(), blogEntry.isPublished());
+        String sqlQuery = String.format("INSERT INTO blog_entry (blog_id,title,content,is_published)\nVALUES('%s','%s','%s','%s');", blogEntry.getBlogEntryId(), blogEntry.getTitle(), blogEntry.getContent(), blogEntry.isPublished());
         BlogEntry createdBlogEntry = null;
         try {
+            String url = "jdbc:postgresql://localhost:5432/blog";
+            Connection conn = DriverManager.getConnection(url, "postgres", "postgres");
             Statement statement = conn.createStatement();
             ResultSet resultSet = statement.executeQuery(sqlQuery);
             if (resultSet.next()) {
                 Statement getStatement = conn.createStatement();
-                ResultSet getResultSet = getStatement.executeQuery("SELECT TOP 1 * FROM blog_entry ORDER BY created_at DESC");
+                ResultSet getResultSet = getStatement.executeQuery("SELECT TOP 1 * FROM blog_entry ORDER BY created_at DESC;");
                 if (getResultSet.next()) {
                     createdBlogEntry = new BlogEntry(
-                            resultSet.getString("blog_entry_id"),
+                            resultSet.getString("id"),
                             resultSet.getTimestamp("created_at").toInstant(),
                             resultSet.getString("blog_id"),
                             resultSet.getString("title"),
@@ -101,18 +94,20 @@ public class BlogEntryService {
     }
 
     public BlogEntry updateBlogEntry(String blogEntryId, BlogEntry blogEntry) {
-        String sqlQuery = String.format("UPDATE blog_entry\nSET blog_id = %s,title = %s,content = %s,is_published =%s\nWHERE blog_entry_id= '%s')",
+        String sqlQuery = String.format("UPDATE blog_entry\nSET blog_id = %s,title = %s,content = %s,is_published =%s\nWHERE id= '%s');",
                 blogEntry.getBlogSiteId(), blogEntry.getTitle(), blogEntry.getContent(), blogEntry.isPublished(), blogEntryId);
         BlogEntry updatedBlogEntry = null;
         try {
+            String url = "jdbc:postgresql://localhost:5432/blog";
+            Connection conn = DriverManager.getConnection(url, "postgres", "postgres");
             Statement statement = conn.createStatement();
             ResultSet resultSet = statement.executeQuery(sqlQuery);
             if (resultSet.next()) {
                 Statement getStatement = conn.createStatement();
-                ResultSet getResultSet = getStatement.executeQuery("SELECT TOP 1 * FROM blog_entry ORDER BY updated_at DESC");
+                ResultSet getResultSet = getStatement.executeQuery("SELECT TOP 1 * FROM blog_entry ORDER BY updated_at DESC;");
                 if (getResultSet.next()) {
                     updatedBlogEntry = new BlogEntry(
-                            resultSet.getString("blog_entry_id"),
+                            resultSet.getString("id"),
                             resultSet.getTimestamp("created_at").toInstant(),
                             resultSet.getString("blog_id"),
                             resultSet.getString("title"),
@@ -127,9 +122,11 @@ public class BlogEntryService {
     }
 
     public void deleteBlogEntry(String blogEntryId) {
-        String sqlQuery = String.format("DELETE FROM blog_entry WHERE blog_entry_id = '%s'", blogEntryId);
+        String sqlQuery = String.format("DELETE FROM blog_entry WHERE id = '%s';", blogEntryId);
 
         try {
+            String url = "jdbc:postgresql://localhost:5432/blog";
+            Connection conn = DriverManager.getConnection(url, "postgres", "postgres");
             Statement statement = conn.createStatement();
             ResultSet resultSet = statement.executeQuery(sqlQuery);
             if (!resultSet.next()) {
